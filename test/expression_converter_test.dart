19297c884371b1cd26e79c021bf57cf87cd98103
Miguel Manjarres <54128874+DevTony101@users.noreply.github.com>
2022-01-16 17:56:47 -0500

    Tests updated

--- test/expression_converter_test.dart
+++ test/expression_converter_test.dart
@@ -4,55 +4,55 @@ import 'package:test/test.dart';
 void main() {
   group('Postfix Conversion', () {
     test('Test 1', () {
-      final result = InfixExpressionConverter(expression: '2*(3+4)/5').toPostfixNotation();
-      expect(result, equals('234+*5/'));
+      final result = InfixExpressionConverter(expression: 'a*(b+c)/d').toPostfixNotation();
+      expect(result, equals('abc+*d/'));
     });
 
     test('Test 2', () {
-      final result = InfixExpressionConverter(expression: '2*3+4*5').toPostfixNotation();
-      expect(result, equals('23*45*+'));
+      final result = InfixExpressionConverter(expression: 'a*b+c*d').toPostfixNotation();
+      expect(result, equals('ab*cd*+'));
     });
 
     test('Test 3', () {
-      final result = InfixExpressionConverter(expression: '2-3+4-5*6').toPostfixNotation();
-      expect(result, equals('23-4+56*-'));
+      final result = InfixExpressionConverter(expression: 'a-b+c-d*e').toPostfixNotation();
+      expect(result, equals('ab-c+de*-'));
     });
 
     test('Test 4', () {
-      final result = InfixExpressionConverter(expression: '(2-3+4)*(5+6*7)').toPostfixNotation();
-      expect(result, equals('23-4+567*+*'));
+      final result = InfixExpressionConverter(expression: '(a-b+c)*(d+e*f)').toPostfixNotation();
+      expect(result, equals('ab-c+def*+*'));
     });
 
     test('Test 5', () {
-      final result = InfixExpressionConverter(expression: '2+3*(4^5-6)^(7+8*9)-1').toPostfixNotation();
-      expect(result, equals('2345^6-789*+^*+1-'));
+      final result = InfixExpressionConverter(expression: 'a+b*(c^d-e)^(f+g*h)-i').toPostfixNotation();
+      expect(result, equals('abcd^e-fgh*+^*+i-'));
     });
   });
 
   group('Prefix Conversion', () {
     test('Test 1', () {
-      final result = InfixExpressionConverter(expression: '2*3+4/5').toPrefixNotation();
-      expect(result, equals('+*23/45'));
+      final result = InfixExpressionConverter(expression: 'a*b+c/d').toPrefixNotation();
+      expect(result, equals('+*ab/cd'));
     });
 
     test('Test 2', () {
-      final result = InfixExpressionConverter(expression: '(2-3/4)*(2/5-6)').toPrefixNotation();
-      expect(result, equals('*-2/34-/256'));
+      final result = InfixExpressionConverter(expression: '(a-b/c)*(d/e-f)').toPrefixNotation();
+      expect(result, equals('*-a/bc-/def'));
     });
 
     test('Test 3', () {
-      final result = InfixExpressionConverter(expression: '2+3*4/5+6').toPrefixNotation();
-      expect(result, equals('++2/*3456'));
+      final result = InfixExpressionConverter(expression: 'a+b*c/d+e').toPrefixNotation();
+      expect(result, equals('++a/*bcde'));
     });
 
     test('Test 4', () {
-      final result = InfixExpressionConverter(expression: '((2+3)+4)-(5-6)^7').toPrefixNotation();
-      expect(result, equals('-++234^-567'));
+      final result = InfixExpressionConverter(expression: '((a+b)+c)-(d-e)^f').toPrefixNotation();
+      expect(result, equals('-++abc^-def'));
     });
 
     test('Test 5', () {
-      final result = InfixExpressionConverter(expression: '(2+3^4)*5+6^7').toPrefixNotation();
-      expect(result, equals('+*+2^345^67'));
+      final result = InfixExpressionConverter(expression: '(a+b^c)*d+e^f').toPrefixNotation();
+      expect(result, equals('+*+a^bcd^ef'));
     });
   });
 }
--- test/expression_evaluator_test.dart
+++ test/expression_evaluator_test.dart
@@ -1,61 +1,71 @@
 import 'dart:math';
 
+import 'package:test/test.dart';
 import 'package:infix_expression_converter/src/converter/infix_expression_converter.dart';
 import 'package:infix_expression_converter/src/evaluator/expression_evaluator.dart';
-import 'package:test/test.dart';
 
 void main() {
   group('Postfix Evaluator', () {
     test('Test 1', () {
-      final result = InfixExpressionConverter(expression: '2*(3+4)/5').toPostfixNotation();
-      expect(ExpressionEvaluator.evaluatePostfix(expression: result), equals(2*(3+4)/5));
+      const context = {'a': 2, 'b': 3, 'c': 4, 'd': 5};
+      final result = InfixExpressionConverter(expression: 'a*(b+c)/d').toPostfixNotation();
+      expect(ExpressionEvaluator.evaluatePostfix(expression: result, context: context), equals(2 * (3 + 4) / 5));
     });
 
     test('Test 2', () {
-      final result = InfixExpressionConverter(expression: '2*3+4*5').toPostfixNotation();
-      expect(ExpressionEvaluator.evaluatePostfix(expression: result), equals(2*3+4*5));
+      const context = {'a': 2, 'b': 10, 'c': 4, 'd': 5};
+      final result = InfixExpressionConverter(expression: 'a*b+c*d').toPostfixNotation();
+      expect(ExpressionEvaluator.evaluatePostfix(expression: result, context: context), equals(2 * 10 + 4 * 5));
     });
 
     test('Test 3', () {
-      final result = InfixExpressionConverter(expression: '2-3+4-5*6').toPostfixNotation();
-      expect(ExpressionEvaluator.evaluatePostfix(expression: result), equals(2-3+4-5*6));
+      const context = {'a': 2, 'b': 3, 'c': 4, 'd': 5, 'e': 6};
+      final result = InfixExpressionConverter(expression: 'a-b+c-d*e').toPostfixNotation();
+      expect(ExpressionEvaluator.evaluatePostfix(expression: result, context: context), equals(2 - 3 + 4 - 5 * 6));
     });
 
     test('Test 4', () {
-      final result = InfixExpressionConverter(expression: '(2-3+4)*(5+6*7)').toPostfixNotation();
-      expect(ExpressionEvaluator.evaluatePostfix(expression: result), equals((2-3+4)*(5+6*7)));
+      const context = {'a': 2, 'b': 3, 'c': 4, 'd': 5, 'e': 6, 'f': 7};
+      final result = InfixExpressionConverter(expression: '(a-b+c)*(d+e*f)').toPostfixNotation();
+      expect(ExpressionEvaluator.evaluatePostfix(expression: result, context: context), equals((2 - 3 + 4) * (5 + 6 * 7)));
     });
 
     test('Test 5', () {
-      final result = InfixExpressionConverter(expression: '2+3*(4^5-6)^(7+8*9)-1').toPostfixNotation();
-      expect(ExpressionEvaluator.evaluatePostfix(expression: result), equals(2+(3*(pow(((pow(4,5))-6),(7+(8*9)))))-1));
+      const context = {'a': 2, 'b': 3, 'c': 4, 'd': 5, 'e': 6, 'f': 7, 'g': 8, 'h': 9, 'i': 1};
+      final result = InfixExpressionConverter(expression: 'a+b*(c^d-e)^(f+g*h)-i').toPostfixNotation();
+      expect(ExpressionEvaluator.evaluatePostfix(expression: result, context: context), equals(2 + (3 * (pow(((pow(4, 5)) - 6), (7 + (8 * 9))))) - 1));
     });
   });
 
   group('Prefix Evaluator', () {
     test('Test 1', () {
-      final result = InfixExpressionConverter(expression: '2*3+4/5').toPrefixNotation();
-      expect(ExpressionEvaluator.evaluatePrefix(expression: result), equals(2*3+4/5));
+      const context = {'a': 2, 'b': 3, 'c': 4, 'd': 5};
+      final result = InfixExpressionConverter(expression: 'a*b+c/d').toPrefixNotation();
+      expect(ExpressionEvaluator.evaluatePrefix(expression: result, context: context), equals(2 * 3 + 4 / 5));
     });
 
     test('Test 2', () {
-      final result = InfixExpressionConverter(expression: '(2-3/4)*(2/5-6)').toPrefixNotation();
-      expect(ExpressionEvaluator.evaluatePrefix(expression: result), equals((2-3/4)*(2/5-6)));
+      const context = {'a': 2, 'b': 3, 'c': 4, 'd': 5, 'e': 6};
+      final result = InfixExpressionConverter(expression: '(a-b/c)*(a/d-e)').toPrefixNotation();
+      expect(ExpressionEvaluator.evaluatePrefix(expression: result, context: context), equals((2 - 3 / 4) * (2 / 5 - 6)));
     });
 
     test('Test 3', () {
-      final result = InfixExpressionConverter(expression: '2+3*4/5+6').toPrefixNotation();
-      expect(ExpressionEvaluator.evaluatePrefix(expression: result), equals(2+3*4/5+6));
+      const context = {'a': 2, 'b': 3, 'c': 4, 'd': 5, 'e': 6};
+      final result = InfixExpressionConverter(expression: 'a+b*c/d+e').toPrefixNotation();
+      expect(ExpressionEvaluator.evaluatePrefix(expression: result, context: context), equals(2 + 3 * 4 / 5 + 6));
     });
 
     test('Test 4', () {
-      final result = InfixExpressionConverter(expression: '((2+3)+4)-(5-6)^7').toPrefixNotation();
-      expect(ExpressionEvaluator.evaluatePrefix(expression: result), equals(((2+3)+4)-(pow(5-6,7))));
+      const context = {'a': 2, 'b': 3, 'c': 4, 'd': 5, 'e': 6, 'f': 7};
+      final result = InfixExpressionConverter(expression: '((a+b)+c)-(d-e)^f').toPrefixNotation();
+      expect(ExpressionEvaluator.evaluatePrefix(expression: result, context: context), equals(((2 + 3) + 4) - (pow(5 - 6, 7))));
     });
 
     test('Test 5', () {
-      final result = InfixExpressionConverter(expression: '(2+3^4)*5+6^7').toPrefixNotation();
-      expect(ExpressionEvaluator.evaluatePrefix(expression: result), equals(((2+(pow(3,4)))*5)+(pow(6,7))));
+      const context = {'a': 2, 'b': 3, 'c': 4, 'd': 5, 'e': 6, 'f': 7};
+      final result = InfixExpressionConverter(expression: '(a+b^c)*d+e^f').toPrefixNotation();
+      expect(ExpressionEvaluator.evaluatePrefix(expression: result, context: context), equals(((2 + (pow(3, 4))) * 5) + (pow(6, 7))));
     });
   });
 }
